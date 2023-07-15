fd4a4fe591dfa32cc9e853b06e16b5826c41a3b2
hotnachos24 <hotnachos24@gmail.com>
2023-07-15 11:49:35 -0700

    added controller input

--- src/main/java/frc/robot/commands/DefaultDriveCommand.java
+++ src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -1,15 +1,15 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class DefaultDriveCommand extends CommandBase {
     
-  final private XboxController controller;
+  final private CommandXboxController controller;
   final private DriveSubsystem driveSubsystem;
 
-  public DefaultDriveCommand(XboxController controller, DriveSubsystem driveSubsystem) {
+  public DefaultDriveCommand(CommandXboxController controller, DriveSubsystem driveSubsystem) {
     this.controller = controller;
     this.driveSubsystem = driveSubsystem;
     this.addRequirements(driveSubsystem);
@@ -18,22 +18,6 @@ public class DefaultDriveCommand extends CommandBase {
   @Override
   public void execute() {
 
-    /* 
-    double turn = joystick.getRawAxis(XboxController.Axis.kRightX.value);
-    double forw = joystick.getRawAxis(XboxController.Axis.kLeftY.value);
-
-    // deadband gamepad 10% 
-    if (Math.abs(forw) < 0.10) {
-      forw = 0;
-    }
-    if (Math.abs(turn) < 0.10) {
-      turn = 0;
-    }
-
-    double leftSpeed = (forw + turn);
-    double rightSpeed = (forw - turn);
-
-    */
     double fwd = controller.getLeftY();
     double rot = controller.getRightX();
 
